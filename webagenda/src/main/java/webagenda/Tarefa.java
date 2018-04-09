089064d50856e6b0cafafdfb4010d3042efd7280
Victor Almeida de Freitas <tc.vfreitas@padtec.com.br>
2018-04-09 10:00:45 -0300

    commit hiber

--- webagenda/src/main/java/webagenda/Tarefa.java
+++ webagenda/src/main/java/webagenda/Tarefa.java
@@ -20,4 +20,38 @@ public class Tarefa {
 	    @Temporal(TemporalType.DATE)
 	    private Calendar dataFinalizacao;
 
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public boolean isFinalizado() {
+			return finalizado;
+		}
+
+		public void setFinalizado(boolean finalizado) {
+			this.finalizado = finalizado;
+		}
+
+		public Calendar getDataFinalizacao() {
+			return dataFinalizacao;
+		}
+
+		public void setDataFinalizacao(Calendar dataFinalizacao) {
+			this.dataFinalizacao = dataFinalizacao;
+		}
+	    
+	    
+
 }
